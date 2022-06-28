71fe03581262ad4f300c26e43aae569bcac1c768
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-06-28 11:25:54 -0300

    Project Created

---
+++ src/main/java/com/educandoweb/CourseeApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/coursee/CourseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
